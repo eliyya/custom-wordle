96119a10bde2a9df0bcf0d452530f46876957128
eliyya <tecno_eli@hotmail.com>
2022-03-11 17:24:38 -0600

    Deploy gh-pages

---
+++ _next/static/hM_r8coY8KeN16Psne_IQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2c620497e059dc15.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
